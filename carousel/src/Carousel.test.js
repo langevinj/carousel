a24aec609b8f5ff1435e22db533948ca30e0e821
Jeremy Langevin <langevinj832@gmail.com>
2020-11-16 11:28:16 -0500

    leftarrow test written and bug fixed

--- carousel/src/Carousel.test.js
+++ carousel/src/Carousel.test.js
@@ -18,6 +18,28 @@ it("works when you click on the right arrow", function() {
   expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
 });
 
+it("works when you click on the left arrow", function() {
+  const { queryByTestId, queryByAltText } = render(<Carousel />);
+
+  //move to second image
+  const rightArrow = queryByTestId("right-arrow");
+  fireEvent.click(rightArrow);
+
+  // expect the second image to show, but not the third
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
+  expect(queryByAltText("Photo by Josh Post on Unsplash")).not.toBeInTheDocument();  
+
+  //move backward in the carousel
+  const leftArrow = queryByTestId("left-arrow");
+  fireEvent.click(leftArrow);
+
+  //expect the first image to show, but not the second
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+
+
+});
+
 //smoke test
 it("renders without crashing", function() {
   render(<Carousel />);
